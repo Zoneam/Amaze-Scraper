6f40a2b414e2466464e87748b2bd1da2574dafa6
Hayk Mnatsakanyan <Zoneam@gmail.com>
2021-10-04 16:23:33 -0700

    scraper initial state

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Hello World
+const PORT = 8000;
+const cheerio = require('cheerio');
+const express = require('express');
+const axios = require('axios');
+const url = 'https://www.amazon.com/s?k=screwdriver&ref=nb_sb_noss_2';
+
+const app = express()
+
+const getData = async () => {
+    const titles = [];
+try {
+    const response = await axios(url);
+    const websiteHtml = response.data;
+    const $ = cheerio.load(websiteHtml)
+
+    $('.a-size-base-plus', websiteHtml).each(function(){
+         titles.push($(this).text())
+
+    })
+console.log(titles)
+} catch(err){
+    console.log(err)
+}
+}
+
+getData();
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+  })
