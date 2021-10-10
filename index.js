fd06c952dfb87489c6b63fe01657fa6cb3a38d63
zoneam <zoneam@gmail.com>
2021-10-09 23:48:38 -0700

    getting data

--- index.js
+++ index.js
@@ -1,45 +1,64 @@
 const PORT = 8000;
-const cheerio = require('cheerio');
-const express = require('express');
-const axios = require('axios');
-const homeUrl = 'https://www.amazon.com';
-const searchPage = 'https://www.amazon.com/s?k=screwdriver&ref=nb_sb_noss_2'
-const app = express()
+const cheerio = require("cheerio");
+const express = require("express");
+const axios = require("axios");
+const { forEach } = require("axios/lib/utils");
+const homeUrl = "https://www.amazon.com";
+const searchPage = "https://www.amazon.com/s?k=screwdriver&ref=nb_sb_noss";
+const app = express();
+let price;
+let urls = [];
+
+const getPrice = async (urls) => {
+  try {
+    urls.forEach(async (url) => {
+      try {
+        const response = await axios(url);
+        const websiteHtml = await response.data;
+        let $ = cheerio.load(websiteHtml);
+        if ($("#priceblock_ourprice", websiteHtml).text()) {
+          price =
+            "ourprice: => " + $("#priceblock_ourprice", websiteHtml).text();
+        } else if ($("#priceblock_dealprice", websiteHtml).text()) {
+          price =
+            "dealerprice: => " + $("#priceblock_dealprice", websiteHtml).text();
+        } else if ($("#priceblock_saleprice", websiteHtml).text()) {
+          price =
+            "saleprice: => " + $("#priceblock_saleprice", websiteHtml).text();
+        }
+        // console.log(pageUrl);
+        console.log(price + "    " + url);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const getData = async () => {
-    let price;
-    let pageUrls = [];
-try {
-const pageResponse = await axios(searchPage);
-let $ = cheerio.load(pageResponse.data);
-    $('.s-no-outline', pageResponse.data).each(function() {
-        const tailLink = $(this).attr('href')
-        pageUrls.push(homeUrl + tailLink);
-    })
-//console.log(pageUrls)
-try {
-// pageUrl = 'https://www.amazon.com/AmazonBasics-Magnetic-Ratchet-Wrench-Screwdriver/dp/B07V843VFN/ref=sr_1_1_sspa?dchild=1&keywords=screwdriver&qid=1633395515&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQTVFQUtUR1g3SkI2JmVuY3J5cHRlZElkPUEwNjE1Mjc0MUtGUVI5WUtaRzQ2VSZlbmNyeXB0ZWRBZElkPUEwMjgwOTA1MVdQSTNEWDIwVlBJTCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
- pageUrls.forEach(async pageUrl => {
-        const response = await axios(pageUrl);
-            const websiteHtml = response.data;
-            $ = cheerio.load(websiteHtml)
-            price = $('#priceblock_ourprice', websiteHtml).text()?$('#priceblock_ourprice', websiteHtml).text():'On Sale Price: ' + $('#priceblock_saleprice', websiteHtml).text()    
-        console.log(price)
- })
-} catch(err){
-    //console.log(err)
-}
+  try {
+    const pageResponse = await axios(searchPage);
+    let $ = cheerio.load(pageResponse.data);
+    $(".s-asin", pageResponse.data).each(function (i) {
+      const tailLink = $(this)
+        .find("a" + ".s-no-outline")
+        .attr("href");
+      if (tailLink) {
+        urls.push(homeUrl + tailLink);
+        // console.log(i + ": " + homeUrl + tailLink);
+      }
+    });
 
-
-
-
-} catch(err){
-   // console.log(err)
-}
-}
+    getPrice(urls);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 getData();
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
-  })
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
