8c13271951ecbb58960dcd2743ade6c2156a9671
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-10-16 01:21:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const PORT = 8000 || process.env.PORT;
+const PORT = process.env.PORT || 8000;
 const cheerio = require("cheerio");
 const express = require("express");
 const path = require("path");
