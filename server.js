b68c561e7c66ec47d8b9d8d6e13e7da184ca0bf6
zoneam <zoneam@gmail.com>
2021-10-16 13:19:38 -0700

    working local

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const axios = require("axios");
 const homeUrl = "https://www.amazon.com";
 const cors = require("cors");
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "/public")));
