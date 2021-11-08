aae86fca16ba512488148bec2224810f97a235e4
Hayk Mnatsakanyan <Zoneam@gmail.com>
2021-11-08 15:46:06 -0800

    changing headers

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/api/walmart/:title", async (req, res) => {
       "Sec-Fetch-Site": "same-origin",
       "Sec-Fetch-Mode": "navigate",
       "Sec-Fetch-Dest": "sequrity",
-      "Referer": "https://www.google.com/",
+      "Referer": "https://www.yahoo.com/",
       "Accept-Encoding": "gzip, deflate, br",
       "Accept-Language": "en-US,en;q=0.9"
     })
